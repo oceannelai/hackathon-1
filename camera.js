797ca39634ddcff886c65838856ed9f6bdac1ac1
Your Name <you@example.com>
2021-11-13 23:04:41 +0400

    project

---
+++ camera.js
@@ -0,0 +1,37 @@
+
+        var video = document.querySelector("video");
+        function startWebCam() {
+            if (navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({ video: true })
+                    .then(function (stream) {
+                        video.srcObject = stream;
+                    })
+                    .catch(function (err0r) {
+                        console.log("Something went wrong!");
+                    });
+            }
+        }
+        startWebCam();
+    
+    //abc drag and drop
+let image = document.getElementById('image');
+let img = document.createElement('img');
+    img.setAttribute('src','/Users/admin/Desktop/hackathon/sunglasses/rayb4.png');
+    image.appendChild(img);
+
+let left =0;
+for(var i=0;i<img.length;i++){
+    let div = document.createElement('div');
+    div.classList.add('box');
+    div.setAttribute('draggable','true');
+    div.innerText =img[i];
+    div.style.left = left + "px";
+    left = left + 51;
+    image.appendChild(div);
+    div.addEventListener('dragend',function(e){
+        let x = e.clientX;  //set coordinate
+        let y = e.clientY;
+        div.style.left = x +'px';
+        div.style.top = y + 'px';
+    })  
+}
---
+++ script.js
@@ -0,0 +1,67 @@
+// searchbar
+function search_products() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('products');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+ 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+// Contact Us
+
+function validate(){
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 3){
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(subject.length < 5){
+    text = "Please Enter Correct Subject";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 8){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 50){
+    text = "Please Enter More Than 50 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
