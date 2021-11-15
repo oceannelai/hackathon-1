83dd8773458836ba0eaefb63667e7edb53a09692
Your Name <you@example.com>
2021-11-15 20:05:12 +0400

    project

--- camera.js
+++ camera.js
@@ -13,25 +13,88 @@
         }
         startWebCam();
     
-    // drag and drop
-let image = document.getElementById('image');
-let img = document.createElement('img');
-    img.setAttribute('src','/Users/admin/Desktop/hackathon/sunglasses/rayb4.png');
-    image.appendChild(img);
+     // drag and drop
 
-let left =0;
-for(var i=0;i<img.length;i++){
-    let div = document.createElement('div');
-    div.classList.add('box');
-    div.setAttribute('draggable','true');
-    div.innerText =img[i];
-    div.style.left = left + "px";
-    left = left + 51;
-    image.appendChild(div);
-    div.addEventListener('dragend',function(e){
-        let x = e.clientX;  //set coordinate
-        let y = e.clientY;
-        div.style.left = x +'px';
-        div.style.top = y + 'px';
-    })  
-}
+let image = document.getElementById('image');
+let str = document.getElementById('myImage');
+
+str.onmousedown = function(event) {
+//by using getBoundingClientRect, it will return the size of the image
+//and position it to the viewport
+  let shiftX = event.clientX - str.getBoundingClientRect().left;
+  let shiftY = event.clientY - str.getBoundingClientRect().top;
+
+ str.style.position = 'absolute';
+  document.body.append(str);
+    str.style.index = 1000;
+  moveAt(event.pageX, event.pageY);
+
+  // moves the image at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+   str.style.left = pageX - shiftX + 'px';
+    str.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the image on mousemove 
+  //so it can be moved from left to top
+  document.addEventListener('mousemove', onMouseMove);
+
+  // drop the image, remove unneeded handlers
+  str.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+   str.onmouseup = null;
+  };
+ };
+//unable the mouse to move the clone of the image
+str.ondragstart = function() {
+  return false;
+};
+
+//image 2
+
+let photo = document.getElementById('main');
+let str2 = document.getElementById('myPhoto');
+
+str2.onmousedown = function(event) {
+//by using getBoundingClientRect, it will return the size of the image
+//and position it to the viewport
+  let shiftX = event.clientX - str2.getBoundingClientRect().left;
+  let shiftY = event.clientY - str2.getBoundingClientRect().top;
+
+ str2.style.position = 'absolute';
+  document.body.append(str2);
+ str2.style.index = 1000;
+  moveAt(event.pageX, event.pageY);
+
+  // moves the image at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+   str2.style.left = pageX - shiftX + 'px';
+    str2.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the image on mousemove 
+  //so it can be moved from left to top
+  document.addEventListener('mousemove', onMouseMove);
+
+  // drop the image, remove unneeded handlers
+  str2.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+   str2.onmouseup = null;
+  };
+ };
+//unable the mouse to move the clone of the image
+str2.ondragstart = function() {
+  return false;
+};
+
+
